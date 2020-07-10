d6edcb4f3506b9be79a12ca1e447bafd8f850b44
Hiren Mistry <hirenmst@gmail.com>
2020-07-10 19:31:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,14 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["Mom", "Dad", "Sister", "Brother"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  family = ["Mom", "Dad", "Sister", "Brother"]
+  family << "Baby Sister"
 end
 
 def add_element_to_start_of_array(array, element)
